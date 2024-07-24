7610c7eb8b31e6c60ba9fb722442a1c58309396f
GitHub Action <action@github.com>
2024-07-24 13:29:08 +0000

    Initialize template repository proper
    Set package name to com.github.trtkojiw.mujmod
    Set project name to Mujmod
    Set modid to mujmod

--- src/main/java/com/github/trtkojiw/mujmod/ExampleMod.java
+++ src/main/java/com/github/trtkojiw/mujmod/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.trtkojiw.mujmod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "mujmod", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
